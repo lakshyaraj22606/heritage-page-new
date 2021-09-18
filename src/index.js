eaa6bfe1fd42111f95d6d8f2ebb9dbfc0a8e1b17
lakshyaraj22606 <2020ucp1827@mnit.ac.in>
2021-09-18 23:53:36 +0530

    myfirst

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+      let more = document.querySelectorAll('.more');
+      for (let i=0; i < more.length; i++){
+    more[i].addEventListener('click',function(){
+      more[i].parentNode.classList.toggle('active')
+    })
+      }
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ src/pages/heritage.js
@@ -0,0 +1,280 @@
+import React from 'react';
+class Heritage extends React.Component{
+    render(){
+        return(
+        <div>
+
+
+    <div class="mainheading">
+        <div class="headingcontent">
+            <h2>HERITAGE SECTION</h2>
+            <p>sub heading</p>
+        </div>
+    </div>
+   
+  <div class="aboutus">
+    <div class="aboutuscontainer">
+      <div class="aboutuscard">  
+        <div class="aboutuscontent">
+          <h1>ABOUT US</h1>
+          <p>A paragraph is a number of sentences grouped together and relating to one topic.
+            Or a group of related sentences that develop a single point. This definition shows
+            that the paragraphs of compositions are not mere arbitrary divisions.
+            The division of a chapter into paragraphs must be made according to the changes of ideas introduced.
+            There is, therefore, no rule as to the length of a paragraph.S
+          </p>
+          <div class="aboutussocial">
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-linkedin"></a>
+          </div> 
+        </div>
+      </div>
+    </div>
+  </div>   
+
+
+  <div class="ouraim">
+  <h1>OUR AIM</h1>
+    <section class="timeline-section">
+      <div class="timeline-items">
+        <div class="timeline-item">
+          <div class="timeline-dot"></div>
+          <div class="timeline-content">
+            <p>A paragraph is a number of sentences grouped together and relating to one topic.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-dot"></div>
+          <div class="timeline-content">
+            <p>A paragraph is a number of sentences grouped together and relating to one topic.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-dot"></div>
+          <div class="timeline-content">
+            <p>A paragraph is a number of sentences grouped together and relating to one topic.</p>
+          </div>
+        </div>
+        <div class="timeline-item">
+          <div class="timeline-dot"></div>
+          <div class="timeline-content">
+            <p>A paragraph is a number of sentences grouped together and relating to one topic.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+
+
+<div class="headerwork">
+    OUR WORKS
+</div>
+<div class="work">
+  <div class="workcontainer">
+    <div class="workcard">
+      <div class="workimage">
+        <img src="assets/images/pro1.jpg"/>
+      </div>
+      <div class="workcontent">
+        <h2>PRO1</h2>
+        <p>A paragraph is a number of sentences grouped together and relating to one topic. Or, a group of related sentences that develop a single point.
+
+          This definition shows that the paragraphs of compositions are not mere arbitrary divisions. The division of a chapter into paragraphs must be made according to the changes of ideas introduced.
+          A paragraph is a number of sentences grouped together and relating to one topic. Or, a group of related sentences that develop a single point.
+        </p>
+      </div>
+      <a class="more"></a>
+    </div>
+    <div class="workcard">
+      <div class="workimage">
+        <img src="assets/images/pro2.jpg"/>
+      </div>
+      <div class="workcontent">
+        <h2>PRO2</h2>
+        <p>A paragraph is a number of sentences grouped together and relating to one topic. Or, a group of related sentences that develop a single point.
+
+          This definition shows that the paragraphs of compositions are not mere arbitrary divisions. The division of a chapter into paragraphs must be made according to the changes of ideas introduced.
+          This definition shows that the paragraphs of compositions are not mere arbitrary divisions. The division of a chapter into paragraphs must be made according to the changes of ideas introduced.
+          A paragraph is a number of sentences grouped together and relating to one topic. Or, a group of related sentences that develop a single point.
+        </p>
+      </div>
+      <a class="more"></a>
+    </div>
+    <div class="workcard">
+      <div class="workimage">
+        <img src="assets/images/pro3.jpg"/>
+      </div>
+      <div class="workcontent">
+        <h2>PRO3</h2>
+        <p>A paragraph is a number of sentences grouped together and relating to one topic. Or, a group of related sentences that develop a single point.
+
+          This definition shows that the paragraphs of compositions are not mere arbitrary divisions. The division of a chapter into paragraphs must be made according to the changes of ideas introduced.
+        </p>
+      </div>
+      <a class="more"></a>
+    </div>   
+  </div>
+</div>
+
+
+<div class="headerwork">
+    TEAM MEMBERS
+  </div>
+  <div class="team-section">
+  <section>
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">sanjay kumar</div>
+          <div class="branch">computer science and engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">sanjay kumar</div>
+          <div class="branch">computer science and engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem2.jpg" />
+      <div class="info">
+        <div class="name">rahul singh</div>
+          <div class="branch">mechanical engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">sanjay kumar</div>
+          <div class="branch">computer science and engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem3.jpg" />
+      <div class="info">
+        <div class="name">priya singh</div>
+          <div class="branch">computer science and engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">sanjay kumar</div>
+          <div class="branch">computer science and engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">sanjay kumar</div>
+          <div class="branch">computer science and engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem2.jpg" />
+      <div class="info">
+        <div class="name">rahul singh</div>
+          <div class="branch">mechanical engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">sanjay kumar</div>
+          <div class="branch">computer science and engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">sanjay kumar</div>
+          <div class="branch">computer science and engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+
+    <div class="team">
+      <img src="assets/images/mem1.jpg" />
+      <div class="info">
+        <div class="name">sanjay kumar</div>
+          <div class="branch">computer science and engineering</div>
+            <div class="social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+            </div>
+      </div>
+    </div>
+        
+  </section>
+
+</div>
+
+
+
+        </div>
+        );
+    }
+}
+
+export default Heritage;
